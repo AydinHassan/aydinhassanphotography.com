e200799f13e8982a9d148be60394091af019d200
Aydin Hassan <aydin@hotmail.co.uk>
2023-03-18 11:23:07 +0100

    Fade component

---
+++ src/components/Fade.vue
@@ -0,0 +1,9 @@
+<template>
+    <Transition name="fade" enter-active-class="transition-opacity duration-300 ease-in"
+                leave-active-class="transition-opacity duration-300 ease-in" enter-from-class="opacity-0"
+                enter-to-class="opacity-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
+        <slot></slot>
+    </Transition>
+</template>
+<script setup>
+</script>
