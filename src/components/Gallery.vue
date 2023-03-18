46816e8d03a1d80fe10caa2502ad9b25a475eff9
Aydin Hassan <aydin@hotmail.co.uk>
2023-03-18 17:54:33 +0100

    Spinner for loading img + better quality images

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -79,6 +79,7 @@ export default {
 
     data() {
         return {
+            selectedImageLoading: false,
             selectedImageIndex: null,
             selectedImage: null,
             previousImage: null,
@@ -147,17 +148,16 @@ export default {
         randomIntBetween: (min, max) => Math.floor(Math.random() * (max - min + 1) + min),
         photoSource(img) {
             if (img.orientation === 'landscape') {
-                return '/photos/' + img.src + '?nf_resize=fit&w=750';
+                return '/photos/' + img.src + '?nf_resize=fit&w=1000';
             }
-            return '/photos/' + img.src + '?nf_resize=fit&h=350';
+            return '/photos/' + img.src + '?nf_resize=fit&h=500';
         },
         photoSourceMain(img) {
-            if (img.orientation === 'landscape') {
-                return '/photos/' + img.src + '?nf_resize=fit&w=1500';
-            }
-            return '/photos/' + img.src + '?nf_resize=fit&h=1000';
+            return '/photos/' + img.src;
         },
         selectImage(img, index) {
+            this.showTitle = false;
+            this.selectedImageLoading = true;
             this.selectedImageIndex = index;
             this.selectedImage = img;
 
@@ -171,7 +171,7 @@ export default {
                 this.nextImage = this.images[index + 1];
             }
 
-            this.setTitleStyles()
+            //TODO: prefetch previous and next image
         },
         selectPreviousImage() {
             if (!this.selectedImage || !this.previousImage) {
@@ -236,14 +236,6 @@ export default {
                 }
             }
         },
-        setTitleStyles() {
-            this.$nextTick(() => {
-                setTimeout(() => {
-                    this.calculateAndShowTitle();
-                }, 60);
-            });
-
-        },
         calculateAndShowTitle() {
             const img = this.$refs.img;
             const container = this.$refs.imgContainer;
@@ -252,9 +244,9 @@ export default {
             const containerRect = container.getBoundingClientRect();
 
             this.imgInfoStyles = {
-                bottom: containerRect.bottom - rect.bottom  + 4 + 'px',
+                bottom: containerRect.bottom - rect.bottom + 'px',
                 left: rect.left,
-                width: (rect.width - 8) + 'px',
+                width: rect.width + 'px',
             }
 
             this.showTitle = true;
@@ -307,6 +299,12 @@ export default {
             this.showInfo = false;
             this.showTitle = false;
         },
+        placeholderSrc(ratio) {
+            const [width, height] = this.calculatePlaceholderSize(ratio);
+
+            return `data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${width * 1000} ${height * 1000}"%3E%3C/svg%3E`;
+        },
+
         onElementObserved(entries) {
             entries.forEach(({ target, isIntersecting}) => {
                 if (isIntersecting) {
@@ -314,6 +312,18 @@ export default {
                     target.classList.remove('opacity-0');
                     this.observer.unobserve(target);
                 }
+            });
+        },
+        calculatePlaceholderSize(ratio) {
+            return ratio.split('/').map((n) => parseInt(n.trim()));
+        },
+        selectedImageLoaded() {
+            this.selectedImageLoading = false;
+
+            this.$nextTick(() => {
+                 setTimeout(() => {
+                     this.calculateAndShowTitle();
+                 }, 60);
             });
         }
     },
@@ -340,14 +350,22 @@ export default {
                     <div class="w-[33px] mx-4 fixed md:static left-1 sm:left-0 my-auto hover:text-orange-400 hover:-translate-x-1 transition duration-300 z-30 text-white text-4xl">
                         <button @click.stop="selectPreviousImage" v-show="previousImage">&larr;</button>
                     </div>
-                    <div ref="imgContainer" class="flex-1 inline-flex justify-center items-center w-full h-full relative p-3 sm:p-0 flex flex-col">
+                    <div ref="imgContainer" class="flex-1 inline-flex justify-center items-center w-full h-full relative p-3 flex flex-col">
 
-                        <img ref="img" loading="lazy" class="border-4 border-white max-h-full w-auto bg-black"
-                             :src="photoSourceMain(selectedImage)"
-                             :alt="selectedImage.title"
-                             :style="imgStyles"
-                             style="aspect-ratio: var(--ratio)"
-                        />
+                        <div :style="imgStyles" style="aspect-ratio: var(--ratio)" class="border-4 border-white bg-black max-h-full max-w-auto">
+                            <div v-show="selectedImageLoading" class="h-screen flex justify-center items-center">
+                                <svg aria-hidden="true" class="inline w-16 h-16 text-gray-200 animate-spin fill-orange-400" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                                </svg>
+                            </div>
+                            <Fade>
+                                <img v-show="!selectedImageLoading" ref="img" class="h-full w-full" @load="selectedImageLoaded"
+                                     :src="photoSourceMain(selectedImage)"
+                                     :alt="selectedImage.title"
+                                />
+                            </Fade>
+                        </div>
 
                         <Fade>
                             <div ref="info" v-show="showTitle || showInfo" class="hidden sm:flex bg-black/70 text-white font-bungee-hairline text-lg absolute justify-between" :style="imgInfoStyles">
