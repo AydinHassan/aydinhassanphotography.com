28b0b9fcd240caee7e097a99cc5a19bc48338d6b
Aydin Hassan <aydin@hotmail.co.uk>
2023-03-22 07:45:12 +0100

    2 col layout mobile, resize album pics + remove lazy loading

--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -9,7 +9,7 @@ export default {
         }
     },
     methods: {
-        photoSource: name => '/photos/' + name,
+        photoSource: name => '/photos/' + name + '?nf_resize=fit&w=500',
     },
 }
 </script>
@@ -18,14 +18,14 @@ export default {
     <div  class="bg-stone-900 w-full flex-1 ">
         <div  class="container lg:w-3/4 mx-auto justify-center flex-wrap overflow-hidden mb-10">
             <h1 class="text-white text-center text-5xl p-4 font-bungee-hairline font-bold my-10">Albums</h1>
-            <div class="grid grid-cols-1 md:grid-cols-3 auto-rows-fr gap-8  p-4">
+            <div class="grid grid-cols-2 md:grid-cols-3 auto-rows-fr gap-8  p-4">
                 <template v-for="(album, albumId) in albums">
                     <div v-if="album.disabled === undefined || !album.disabled"  class="flex flex-col items-center justify-end group hover:cursor-pointer ">
                         <div class="flex-grow flex items-center">
                             <h3 class="text-center text-2xl text-white font-bungee-hairline p-3 group-hover:text-orange-300"><router-link :to="{ name: 'album', params: {'id' : albumId}}">{{ album.title }}</router-link></h3>
                         </div>
                         <router-link :to="{ name: 'album', params: {'id' : albumId}}">
-                            <img loading="lazy" :src="photoSource(album.cover)" :alt="album.title" class="aspect-square object-cover opacity-1 transition-opacity duration-200 ease-in group-hover:opacity-60">
+                            <img :src="photoSource(album.cover)" :alt="album.title" class="aspect-square object-cover opacity-1 transition-opacity duration-200 ease-in group-hover:opacity-60">
                         </router-link>
                     </div>
                 </template>
