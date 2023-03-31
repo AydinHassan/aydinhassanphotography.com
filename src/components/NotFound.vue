7b217e29ad0307f82f100af5ae9ec8c8e5baf85d
Aydin Hassan <aydin@hotmail.co.uk>
2023-03-31 08:38:24 +0200

    Fix repeated word

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -13,7 +13,7 @@ export default {
     <div class="bg-stone-900 w-full flex-1 overflow-y-hidden overflow-y-scroll">
         <div class="container lg:w-3/4 mx-auto flex flex-col items-center mb-10">
             <h1 class="text-white text-center text-5xl p-4 font-bungee-hairline font-bold mt-10">Not found</h1>
-            <p class="text-white text-center text-base p-4 font-bungee-hairline font-bold mb-4">You've strayed off the path. Good things things usually come to those who do. In this instance, just go <router-link class="text-orange-400 hover:underline" to="/">home</router-link>.</p>
+            <p class="text-white text-center text-base p-4 font-bungee-hairline font-bold mb-4">You've strayed off the path. Good things usually come to those who do. In this instance, just go <router-link class="text-orange-400 hover:underline" to="/">home</router-link>.</p>
             <img class="w-2/3" alt="Not found" :src="bgSrc"/>
         </div>
     </div>
