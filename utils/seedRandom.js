ad17eae61f8fa23eaa72f5c211edd778138b72d9
Aydin Hassan <aydin@hotmail.co.uk>
2023-05-10 23:02:53 +0200

    Do not include hours in seed

--- utils/seedRandom.js
+++ utils/seedRandom.js
@@ -2,7 +2,6 @@ import seedrandom from 'seedrandom'
 
 export const generateSeed = () => {
     const now = new Date()
-    const seed = `${now.getFullYear()}${now.getMonth()}${now.getDate()}${now.getHours()}`
-    console.log('SEED: ' + seed);
+    const seed = `${now.getFullYear()}${now.getMonth()}${now.getDate()}`
     return seedrandom(seed);
 }
