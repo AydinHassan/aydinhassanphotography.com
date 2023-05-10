224a39abfe6f90ace848e2d30b626413f9a32c38
Aydin Hassan <aydin@hotmail.co.uk>
2023-05-10 22:59:03 +0200

    Log seed

--- utils/seedRandom.js
+++ utils/seedRandom.js
@@ -3,5 +3,6 @@ import seedrandom from 'seedrandom'
 export const generateSeed = () => {
     const now = new Date()
     const seed = `${now.getFullYear()}${now.getMonth()}${now.getDate()}${now.getHours()}`
+    console.log('SEED: ' + seed);
     return seedrandom(seed);
 }
